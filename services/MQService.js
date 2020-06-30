cea498d6e533f1589fe69f1bacf7091d60ad58fc
Santu Koley <santu@geogo.in>
2020-06-30 08:18:32 +0000

    Update MQService.js. Change back to false

--- services/MQService.js
+++ services/MQService.js
@@ -18,7 +18,7 @@ amqp.connect(CONN_URL, function (err, conn) {
       throw err;
     }
     channel.assertQueue(RABBITMQ_CHANNEL, {
-      durable: true
+      durable: false
     });
     ch = channel;
   });
